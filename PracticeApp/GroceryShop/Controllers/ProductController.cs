a3896d5a486956ef1af8f8d39b618a30abd34396
Gemma <jemmimah.abdul@decagon.dev>
2023-11-09 23:27:48 +0100

    Updated Project

--- PracticeApp/GroceryShop/Controllers/ProductController.cs
+++ PracticeApp/GroceryShop/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using GroceryShopCore.IServices;
+using GroceryShopData.DTO;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GroceryShop.Controllers
@@ -7,5 +9,40 @@ namespace GroceryShop.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
+        private readonly IProductRepository _productRepository;
+
+        [HttpPost("CreateProduct")]
+        public IActionResult CreateProduct(ProductDTO productDTO)
+        {
+            _productRepository.CreateProduct(productDTO);
+            return Ok("Product created Successfully");
+        }
+
+        [HttpPut("UpdateProduct")]
+        public IActionResult EditProductById(string productId, ProductDTO productDTO)
+        {
+            _productRepository.UpdateProduct(productId, productDTO);
+            return Ok("Updated successfully");
+
+        }
+        [HttpGet("GetProductById")]
+        public IActionResult GetProductById(string productId)
+        {
+            var product = _productRepository.GetProductById(productId);
+            return Ok(product);
+        }
+
+        [HttpGet("GetAllProduct")]
+        public IActionResult GetAllProduct()
+        {
+            var product = _productRepository.GetAllProduct();
+            return Ok(product);
+        }
+        [HttpDelete("Delete")]
+        public IActionResult DeleteProduct(string productId) 
+        { 
+            _productRepository.DeleteProduct(productId);
+            return Ok("Product deleted successfully");
+        }
     }
 }
