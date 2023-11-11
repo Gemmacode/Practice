b624c1c0f346eca440c1a30436bce1df9af29760
Gemma <jemmimah.abdul@decagon.dev>
2023-11-11 20:41:05 +0100

    Updated Project

--- OnlineFastFoodDelivery/FastFood/Program.cs
+++ OnlineFastFoodDelivery/FastFood/Program.cs
@@ -1,7 +1,11 @@
+using FastFood.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options=> options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContextConnection")));
 
 var app = builder.Build();
 
