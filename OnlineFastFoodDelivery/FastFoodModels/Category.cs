15e74e4695445ac32a2bae2b7a065556137662fa
Gemma <jemmimah.abdul@decagon.dev>
2023-11-11 14:49:25 +0100

    Updated Project

--- OnlineFastFoodDelivery/FastFoodModels/Category.cs
+++ OnlineFastFoodDelivery/FastFoodModels/Category.cs
@@ -1,12 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FastFood.Models
 {
-    internal class Category
+    public class Category
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
     }
 }
--- OnlineFastFoodDelivery/FastFoodModels/Item.cs
+++ OnlineFastFoodDelivery/FastFoodModels/Item.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace FastFood.Models
 {
-    internal class Item
+    public class Item
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int SubCategoryId { get; set; }  
+        public SubCategory SubCategory { get; set; }
+
     }
 }
